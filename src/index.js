5560d89239e8b49fdef32e536f67774a6829839b
Emmanuel Adekunle <eadekunle56@gmail.com>
2025-05-08 14:02:38 +0100

    Fix navbar toggle and mobile spacing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './theme.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';

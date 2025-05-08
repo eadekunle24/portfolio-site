1a6cfdb4d14eface5d338a15909c2a222c6a284c
Emmanuel Adekunle <eadekunle56@gmail.com>
2025-05-08 14:37:11 +0100

    Fix AOS initialization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-AOS.init();
+import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Navbar from './components/Navbar';
-import React from 'react';
-import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -11,16 +9,14 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './custom.css';
 
-
 function App() {
+  useEffect(() => {
+    AOS.init({ once: true });
+  }, []);
+
   return (
     <>
-      <>
-  <Navbar />
-  {/* Other components like Hero, About, Projects, etc */}
-</>
-
-      <Header />
+      <Navbar />
       <Hero />
       <About />
       <Projects />
